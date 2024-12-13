4ea089ead7879dae17d90e000d85e67996ed1e0c
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2024-12-13 06:36:36 -0300

    AA

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,33 @@
+import { View, Text, Pressable } from "react-native"
+import { Ionicons, Feather } from '@expo/vector-icons'
+
+
+export default function Header() {
+    return (
+        <View className=" w-full flex flex-row items-center justify-between">
+            <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+                <Ionicons name="menu" size={20} color='#121212'></Ionicons>
+            </Pressable>
+
+            <View className="flex flex-col items-center justify-center">
+                <Text className="text-center text-sm text-slate-700">Localização</Text>
+
+                <View className="flex-row items-center justify-center gap-1">
+                    <Feather 
+                        name="map-pin"
+                        size={14} 
+                        color={'#ff0000'}
+                    />
+                    <Text className="text-lg font-bold">
+                        Campo grande
+                    </Text>
+                </View>
+
+            </View>
+            
+            <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+                <Feather name="bell" size={20} color='#121212'></Feather>
+            </Pressable>
+        </View>
+    )
+}
